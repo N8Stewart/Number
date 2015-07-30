f45ca3e891c8310a516ce59e17aabb85a19124ee
N8Stewart <Nate.Stewart12@gmail.com>
2015-07-29 22:06:53 -0700

    Remove dependency on junit, and add dependency on TestNG.

--- test/DigitTest.java
+++ test/DigitTest.java
@@ -1,32 +1,27 @@
-package number.test;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import number.Digit;
 import number.exception.DigitException;
-
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DigitTest {
 	
 	/**
 	 * Ensure trash values can't become digits through the constructor.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_default_constructor(){
 		Digit digit = new Digit(); //This should be 0
-		assertTrue(digit.isZero());
+		Assert.assertTrue(digit.isZero());
 	}
 	
 	/**
 	 * Ensure trash values can't become digits through the constructor.
 	 */
-	@Test
-	public void test_constructor_a(){
+	@Test(groups = {"digit"})
+	public void test_constructor_a() {
 		try {
 			Digit digit = new Digit('a');
-			fail("Digit a is not a valid digit.");
+			Assert.fail("Digit a is not a valid digit.");
 		} catch (DigitException e) {
 			// It should throw an exception.
 		}
@@ -35,11 +30,11 @@ public class DigitTest {
 	/**
 	 * Ensure trash values can't become digits through the constructor.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_A(){
 		try {
 			Digit digit = new Digit('A');
-			fail("Digit A is not a valid digit.");
+			Assert.fail("Digit A is not a valid digit.");
 		} catch (DigitException e) {
 			// It should throw an exception.
 		}
@@ -48,11 +43,11 @@ public class DigitTest {
 	/**
 	 * Ensure trash values can't become digits through the constructor.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_dash(){
 		try {
 			Digit digit = new Digit('-');
-			fail("Digit - is not a valid digit.");
+			Assert.fail("Digit - is not a valid digit.");
 		} catch (DigitException e) {
 			// It should throw an exception.
 		}
@@ -61,11 +56,11 @@ public class DigitTest {
 	/**
 	 * Ensure trash values can't become digits through the constructor.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_exclamation(){
 		try {
 			Digit digit = new Digit('!');
-			fail("Digit ! is not a valid digit.");
+			Assert.fail("Digit ! is not a valid digit.");
 		} catch (DigitException e) {
 			// It should throw an exception.
 		}
@@ -74,49 +69,49 @@ public class DigitTest {
 	/**
 	 * Ensure correct digits are parsed correctly
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_0(){
 		try {
 			Digit digit = new Digit('0');
 			// Should not throw an exception
-			
+
 		} catch (DigitException e) {
-			fail("Digit 0 is a valid digit.");
+			Assert.fail("Digit 0 is a valid digit.");
 		}
 	}
 	
 	/**
 	 * Ensure correct digits are parsed correctly
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_1(){
 		try {
 			Digit digit = new Digit('1');
 			// Should not throw an exception
 			
 		} catch (DigitException e) {
-			fail("Digit 1 is a valid digit.");
+			Assert.fail("Digit 1 is a valid digit.");
 		}
 	}
 	
 	/**
 	 * Ensure correct digits are parsed correctly
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_constructor_9(){
 		try {
 			Digit digit = new Digit('9');
 			// Should not throw an exception
 			
 		} catch (DigitException e) {
-			fail("Digit 9 is a valid digit.");
+			Assert.fail("Digit 9 is a valid digit.");
 		}
 	}
 	
 	/**
 	 * Ensure the digit is set correctly before an after the call to set digit.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_setDigit_1_from_0(){
 		
 		Digit digit = null;
@@ -124,16 +119,16 @@ public class DigitTest {
 		try {
 			// Create digit and make sure it is set correctly
 			digit = new Digit('0');
-			assertTrue(digit.getDigit() == '0');
+			Assert.assertTrue(digit.getDigit() == '0');
 		} catch (DigitException e) {
-			fail("Problem creating digit");
+			Assert.fail("Problem creating digit");
 		}
 		
 		try{
 			digit.setDigit('1');
-			assertTrue(digit.getDigit() == '1');
+			Assert.assertTrue(digit.getDigit() == '1');
 		} catch (DigitException e) {
-			fail("Problem setting valid digit.");
+			Assert.fail("Problem setting valid digit.");
 		}
 		
 	}
@@ -141,7 +136,7 @@ public class DigitTest {
 	/**
 	 * Ensure the digit is set correctly before an after the call to set digit.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_setDigit_9_from_8(){
 		
 		Digit digit = null;
@@ -149,16 +144,16 @@ public class DigitTest {
 		try {
 			// Create digit and make sure it is set correctly
 			digit = new Digit('8');
-			assertTrue(digit.getDigit() == '8');
+			Assert.assertTrue(digit.getDigit() == '8');
 		} catch (DigitException e) {
-			fail("Problem creating digit");
+			Assert.fail("Problem creating digit");
 		}
 		
 		try{
 			digit.setDigit('9');
-			assertTrue(digit.getDigit() == '9');
+			Assert.assertTrue(digit.getDigit() == '9');
 		} catch (DigitException e) {
-			fail("Problem setting valid digit.");
+			Assert.fail("Problem setting valid digit.");
 		}
 		
 	}
@@ -166,7 +161,7 @@ public class DigitTest {
 	/**
 	 * Ensure the digit is set correctly before an after the call to set digit.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_setDigit_a_from_0(){
 		
 		Digit digit = null;
@@ -174,14 +169,14 @@ public class DigitTest {
 		try {
 			// Create digit and make sure it is set correctly
 			digit = new Digit('0');
-			assertTrue(digit.getDigit() == '0');
+			Assert.assertTrue(digit.getDigit() == '0');
 		} catch (DigitException e) {
-			fail("Problem creating digit");
+			Assert.fail("Problem creating digit");
 		}
 		
 		try{
 			digit.setDigit('a');
-			fail("Exception wasn't thrown for invalid digit.");
+			Assert.fail("Exception wasn't thrown for invalid digit.");
 		} catch (DigitException e) {
 			// We expect an exception to arise here.
 		}
@@ -191,7 +186,7 @@ public class DigitTest {
 	/**
 	 * Ensure the digit is set correctly before an after the call to set digit.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_setDigit_dollar_from_0(){
 		
 		Digit digit = null;
@@ -199,14 +194,14 @@ public class DigitTest {
 		try {
 			// Create digit and make sure it is set correctly
 			digit = new Digit('0');
-			assertTrue(digit.getDigit() == '0');
+			Assert.assertTrue(digit.getDigit() == '0');
 		} catch (DigitException e) {
-			fail("Problem creating digit");
+			Assert.fail("Problem creating digit");
 		}
 		
 		try{
 			digit.setDigit('$');
-			fail("Exception wasn't thrown for invalid digit.");
+			Assert.fail("Exception wasn't thrown for invalid digit.");
 		} catch (DigitException e) {
 			// We expect an exception to arise here.
 		}
@@ -216,19 +211,18 @@ public class DigitTest {
 	/**
 	 * Test if isDigit works correctly in deciding if something is a digit or not
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_isDigit(){
-		assertTrue(Digit.isDigit('0'));
-		assertTrue(Digit.isDigit('1'));
-		assertTrue(Digit.isDigit('9'));
-		assertFalse(Digit.isDigit('a'));
-		assertTrue(Digit.isDigit(1));
-		assertTrue(Digit.isDigit(0));
-		assertTrue(Digit.isDigit(9));
-		assertFalse(Digit.isDigit(20));
-		assertFalse(Digit.isDigit(-1));
-		assertFalse(Digit.isDigit('&'));
-		
+		Assert.assertTrue(Digit.isDigit('0'));
+		Assert.assertTrue(Digit.isDigit('1'));
+		Assert.assertTrue(Digit.isDigit('9'));
+		Assert.assertFalse(Digit.isDigit('a'));
+		Assert.assertTrue(Digit.isDigit(1));
+		Assert.assertTrue(Digit.isDigit(0));
+		Assert.assertTrue(Digit.isDigit(9));
+		Assert.assertFalse(Digit.isDigit(20));
+		Assert.assertFalse(Digit.isDigit(-1));
+		Assert.assertFalse(Digit.isDigit('&'));
 	}
 	
 	
@@ -236,43 +230,43 @@ public class DigitTest {
 	/**
 	 * Test if the valid digit 0 is even.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_isEven_0(){
 		try {
-			assertTrue(new Digit('0').isEven());
+			Assert.assertTrue(new Digit('0').isEven());
 		} catch (DigitException e) {
-			fail("The digit 0 is a  digit.");
+			Assert.fail("The digit 0 is a  digit.");
 		}
 	}
 	
 	/**
 	 * Test if the valid digit 1 is even.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_isEven_1(){
 		try {
-			assertFalse(new Digit('1').isEven());
+			Assert.assertFalse(new Digit('1').isEven());
 		} catch (DigitException e) {
-			fail("The digit 1 is a digit");
+			Assert.fail("The digit 1 is a digit");
 		}
 	}
 	
 	/**
 	 * Test if the valid digit 2 is even.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_isEven_2(){
 		try {
-			assertTrue(new Digit('2').isEven());
+			Assert.assertTrue(new Digit('2').isEven());
 		} catch (DigitException e) {
-			fail("The digit 2 is a  digit.");
+			Assert.fail("The digit 2 is a  digit.");
 		}
 	}
 	
 	/**
 	 * Compare the digit 0 to 1. Should return a negative value since 0 < 1.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_compareTo_0_1(){
 		
 		Digit first = null, second = null;
@@ -282,16 +276,16 @@ public class DigitTest {
 			first = new Digit('0');
 			second = new Digit('1');
 		} catch (DigitException e) {
-			fail("The digits are valid.");
+			Assert.fail("The digits are valid.");
 		}
-		
-		assertTrue(first.compareTo(second) < 0);
+
+		Assert.assertTrue(first.compareTo(second) < 0);
 	}
 	
 	/**
 	 * Compare the digit 1 to 0. Should return a negative value since 1 > 0.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_compareTo_1_0(){
 		
 		Digit first = null, second = null;
@@ -301,16 +295,16 @@ public class DigitTest {
 			first = new Digit('1');
 			second = new Digit('0');
 		} catch (DigitException e) {
-			fail("The digits are valid.");
+			Assert.fail("The digits are valid.");
 		}
-		
-		assertTrue(first.compareTo(second) > 0);
+
+		Assert.assertTrue(first.compareTo(second) > 0);
 	}
 	
 	/**
 	 * Compare the digit 0 to 0. Should return a negative value since 0 == 0.
 	 */
-	@Test
+	@Test(groups = {"digit"})
 	public void test_compareTo_0_0(){
 		
 		Digit first = null, second = null;
@@ -320,10 +314,10 @@ public class DigitTest {
 			first = new Digit('0');
 			second = new Digit('0');
 		} catch (DigitException e) {
-			fail("The digits are valid.");
+			Assert.fail("The digits are valid.");
 		}
-		
-		assertTrue(first.compareTo(second) == 0);
+
+		Assert.assertTrue(first.compareTo(second) == 0);
 	}
 
 }
--- test/NaturalNumberTest.java
+++ test/NaturalNumberTest.java
@@ -1,19 +1,15 @@
-package number.test;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import number.NaturalNumber;
 import number.exception.DigitException;
 import number.exception.NaturalNumberException;
-
-import org.junit.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class NaturalNumberTest {
 	
 	/**
 	 * Ensure a deque is correctly established to hold the digits
 	 */
-	@Test
+	@Test(groups = {"natural_number"})
 	public void test_Constructor(){
 		
 		NaturalNumber num = null;
@@ -23,15 +19,14 @@ public class NaturalNumberTest {
 		try {
 			num = new NaturalNumber(rep);
 		} catch (DigitException e) {
-			fail("All digits are valid.");
+			Assert.fail("All digits are valid.");
 		} catch (NaturalNumberException e) {
-			fail("Number is valid.");
+			Assert.fail("Number is valid.");
 		}
-		
+
 		// Convert the number back to a string and make sure it is the same
-		System.out.println(num.toString());
-		assertTrue(num.toString() == rep);
-		
+		Assert.assertEquals("String representation does not match expected.", rep, num.toString());
+
 	}
 
 }
