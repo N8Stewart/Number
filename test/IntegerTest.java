5f059900586a17d84b2c8d130249371f4b35eca6
N8Stewart <Nate.Stewart12@gmail.com>
2015-08-21 19:04:56 -0400

    Added tests for constructor methods. Closes #7.

--- test/IntegerTest.java
+++ test/IntegerTest.java
@@ -53,4 +53,164 @@ public class IntegerTest {
 
     }
 
+    /**
+     * Ensure a integer with value '0' is created using the default constructor
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_default_constructor() throws DigitException {
+
+        Integer integer = new Integer();
+        Assert.assertTrue(integer.isZero(), "Default constructor instantiated non-zero integer");
+
+    }
+
+    /**
+     * Ensure a positive number can be copied correctly using the copy constructor
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_copy_constructor_positive() throws DigitException {
+
+        final String rep = "5678904596877896458756467";
+
+        Integer integerSource = new Integer(rep);
+        Integer integerDest = new Integer(integerSource);
+        Assert.assertEquals(integerDest.toString(), rep, "Copy constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a negative number can be copied correctly using the copy constructor
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_copy_constructor_negative() throws DigitException {
+
+        final String rep = "-345678987654456787654345678";
+
+        Integer integerSource = new Integer(rep);
+        Integer integerDest = new Integer(integerSource);
+        Assert.assertEquals(integerDest.toString(), rep, "Copy constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a zero can be copied correctly using the copy constructor
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_copy_constructor_zero() throws DigitException {
+
+        final String rep = "0";
+
+        Integer integerSource = new Integer(rep);
+        Integer integerDest = new Integer(integerSource);
+        Assert.assertEquals(integerDest.toString(), rep, "Copy constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_positive1() throws DigitException {
+
+        final String rep = "00000001234567890";
+        final String expectedRep = "1234567890";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), expectedRep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_positive2() throws DigitException {
+
+        final String rep = "1";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_positive3() throws DigitException {
+
+        final String rep = "0";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_positive4() throws DigitException {
+
+        final String rep = "12345678998765456789999008765456765434567876545678765434567897654567";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_negative1() throws DigitException {
+
+        final String rep = "-12345678998765456789999008765456765434567876545678765434567897654567";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_negative2() throws DigitException {
+
+        final String rep = "-1";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_negative3() throws DigitException {
+
+        final String rep = "-000000000004";
+        final String expectedRep = "-4";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), expectedRep, "Constructor instantiated incorrect integer.");
+
+    }
+
+    /**
+     * Ensure a deque can be properly generated for the passed in string
+     */
+    @Test(groups = {"integer", "unit"})
+    public void test_constructor_negative() throws DigitException {
+
+        final String rep = "-1337";
+
+        Integer integer = new Integer(rep);
+        Assert.assertEquals(integer.toString(), rep, "Constructor instantiated incorrect integer.");
+
+    }
+
+
 }
