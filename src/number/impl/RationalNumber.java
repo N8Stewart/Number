0b9cd5dbf2edc792d528577ea67b52d13f1a659d
N8Stewart <Nate.Stewart12@gmail.com>
2015-08-27 23:05:03 -0400

    Added initial framework for the RationalNumber implementation.

--- src/number/impl/RationalNumber.java
+++ src/number/impl/RationalNumber.java
@@ -1,5 +1,40 @@
 package number.impl;
 
-public class RationalNumber {
+import number.Number;
+import number.attribute.Positivity;
+import number.attribute.Zeroable;
+import number.exception.DigitException;
+import number.exception.NaturalNumberException;
+
+public class RationalNumber implements Positivity, Zeroable, number.Number {
+
+    private Integer numerator;
+    private NaturalNumber denominator;
+
+    public RationalNumber() {
+
+        try {
+            this.numerator = new Integer("0");
+            this.denominator = new NaturalNumber("1");
+        } catch (DigitException | NaturalNumberException e) {
+            // This will never occur unless the Integer or NaturalNumber constructors have been modified
+        }
+
+    }
+
+    @Override
+    public boolean isPositive() {
+        return this.numerator.isPositive();
+    }
+
+    @Override
+    public boolean isZero() {
+        return this.numerator.isZero();
+    }
+
+    @Override
+    public int compareTo(Number o) {
+        return 0;
+    }
 
 }
