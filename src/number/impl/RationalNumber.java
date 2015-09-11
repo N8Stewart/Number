3f184eff173a87a7fc4124825abfb59247934d5e
N8Stewart <Nate.Stewart12@gmail.com>
2015-09-10 22:55:01 -0400

    Add two constructors.

--- src/number/impl/RationalNumber.java
+++ src/number/impl/RationalNumber.java
@@ -5,12 +5,16 @@ import number.attribute.Positivity;
 import number.attribute.Zeroable;
 import number.exception.DigitException;
 import number.exception.NaturalNumberException;
+import number.exception.RationalNumberException;
 
 public class RationalNumber implements Positivity, Zeroable, number.Number {
 
     private Integer numerator;
     private NaturalNumber denominator;
 
+    /**
+     * Create a RationalNumber with value of 0.
+     */
     public RationalNumber() {
 
         try {
@@ -19,6 +23,31 @@ public class RationalNumber implements Positivity, Zeroable, number.Number {
         } catch (DigitException | NaturalNumberException e) {
             // This will never occur unless the Integer or NaturalNumber constructors have been modified
         }
+
+    }
+
+    /**
+     * Construct a RationalNumber from a string representation
+     *
+     * @param number a string representation of a rational number
+     * @throws RationalNumberException when a rational number
+     */
+    public RationalNumber(final String number) throws RationalNumberException {
+
+
+
+    }
+
+    /**
+     * Construct a RationalNumber from a numerator and denominator.
+     *
+     * @param numerator   an Integer value represents the numerator for a rational number
+     * @param denominator a NaturalNumber represents the denominator for a rational number
+     */
+    public RationalNumber(final Integer numerator, final NaturalNumber denominator) {
+
+        this.numerator = numerator;
+        this.denominator = denominator;
 
     }
 
